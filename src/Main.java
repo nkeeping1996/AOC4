25f312b33898f22b53ceca18caa9ae0016f34395
nkeeping1996 <nfk.keeping@gmail.com>
2022-12-04 09:12:35 -0500

    Final Solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        int count = 0;
+        File f = new File("src/AOC4Data");
+        FileReader fd = new FileReader(f);
+        BufferedReader br = new BufferedReader(fd);
+        String line;
+        //Puzzle1
+//        while ((line = br.readLine()) != null) {
+//            String[] secs = line.split("[,-]");
+//            if(Integer.parseInt(secs[0])<=Integer.parseInt(secs[1])&&Integer.parseInt(secs[1])>=Integer.parseInt(secs[3])){
+//                count++;
+//            }
+//            else if(Integer.parseInt(secs[0])>=Integer.parseInt(secs[2])&&Integer.parseInt(secs[1])<=Integer.parseInt(secs[3])){
+//                count++;
+//            }
+//        }
+        //Puzzle2
+        while ((line = br.readLine()) != null) {
+            String[] secs = line.split("[,-]");
+            if(Integer.parseInt(secs[1])>=Integer.parseInt(secs[2])&&Integer.parseInt(secs[0])<=Integer.parseInt(secs[3])){
+                count++;
+            }
+        }
+        System.out.println(count);
     }
+
 }
